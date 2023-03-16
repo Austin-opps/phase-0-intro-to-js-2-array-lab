318d217c0a41bd0cf9a985b9adde5b43fb0735b1
Austin <akomen04@gmail.com>
2023-03-17 00:59:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+let cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+    return cats;
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+    return cats;
+}
+
+// not to include Garfield
+function destructivelyRemoveLastCat(Garfield) {
+    cats.pop(Garfield);
+    return cats;
+}
+
+// not to include Milo
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+    return cats;
+}
+
+function appendCat(Broom) {
+    var members=[...cats, Broom];
+    return members;
+}
+
+function prependCat(Arnold) {
+    var members=[Arnold,...cats];
+    return members;
+}
+
+// remove Garfield
+function removeLastCat() {
+    var members=cats.slice(0,cats.length-1)
+    return members
+}
+
+function removeFirstCat() {
+    var members=cats.slice(1)
+    return members;
+}
